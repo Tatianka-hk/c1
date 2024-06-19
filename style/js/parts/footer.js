5428ffbf7b6b96fc878813157922b865385ff7d4
Tatianka-hk <78912196+Tatianka-hk@users.noreply.github.com>
2024-06-19 12:44:54 +0200

    Update footer.js

--- style/js/parts/footer.js
+++ style/js/parts/footer.js
@@ -1,1 +1,1 @@
-const footerHTML = "  <footer><img src='./style/img/logo.png' alt='Logo'><div class='contact_us'> <div>sdsdfsdfsdf : ssdfsdfsdfsdf</div><div>sdsdfsdfsdf : ssdfsdfsdfsdf</div></div></footer>"
+const footerHTML = "  <footer><img src='./style/img/logo.png' alt='Logo'><div class='contact_us'> <div>facebook : ГО "Наука"</div><div>instagram : @gonauka</div></div></footer>"
